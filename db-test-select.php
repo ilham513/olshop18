c57d2164f3c42e5c366556a9464fbd14be8dce3e
Setia Bhakti <joebhakti513@gmail.com>
2021-03-31 14:53:04 +0700

    Create db-test-select.php

---
+++ db-test-select.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+  
+$db = parse_url(getenv("DATABASE_URL"));
+$db["path"] = ltrim($db["path"], "/");
+
+// konfigurasi koneksi
+$host       =  $db["host"];
+$dbuser     =  $db["user"];
+$dbpass     =  $db["pass"];
+$port       =  $db["port"];
+$dbname    =  $db["path"];
+
+try {
+    $conn = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id FROM love");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
+
+</body>
+</html>
